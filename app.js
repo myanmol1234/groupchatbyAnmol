828df89c22fdfdfdaa0f3d6cd33e2d765317f1d4
myanmol1234 <anmolgoyal981997@gmail.com>
2022-10-05 22:52:57 +0530

    app.js updated as per routes added

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const app=express();
 var cors = require('cors');
 app.use(cors());
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const getroutes=require('./routes/admin')
-const sequelize = require('./utili/database');
-const groupRoute = require('./routes/admin');
+const userRoute=require('./routes/user')
+const messageRoute=require('./routes/message')
+const groupRoute=require('./routes/group')
+
+const User=require('./models/user')
+const Message=require('./models/message')
+const Group=require('./models/group')
 //app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json())
 
-app.use(adminRoutes);
-app.use(getroutes);
+app.use(userRoute);
+app.use(messageRoute);
 app.use(groupRoute);
+
+
 
 sequelize
   .sync()
